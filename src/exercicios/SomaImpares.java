9df5d487d258e7bb90e347cd79d8dd1632ba39b7
Eduardo-Aragao <eduardo.aragaosoft@outlook.com>
2026-02-04 16:46:34 -0400

    feat: adiciona exercício de soma de ímpares entre os 2 números

---
+++ src/exercicios/SomaImpares.java
@@ -0,0 +1,35 @@
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SomaImpares {
+
+    public static void main(String []args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int x, y, somaImpares, troca;
+
+        System.out.println("Digite dois numeros: ");
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        if (x > y) {
+            troca = x;
+            x = y;
+            y = troca;
+        }
+
+        somaImpares = 0;
+
+        for(int i = x+1; i < y; i++){
+            if ( i % 2 != 0){
+                somaImpares = somaImpares + i;
+            }
+        }
+
+        System.out.println("SOMA DOS IMPARES = " + somaImpares);
+        sc.close();
+    }
+}
